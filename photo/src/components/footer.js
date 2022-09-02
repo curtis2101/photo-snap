5b3bd601043e46b7eb8898ffc6368680693dc75e
Curtis Sanderson <cw1sander@gmail.com>
2022-09-02 11:22:53 +0200

    changed footer to grid for mobile

--- photo/src/components/footer.js
+++ photo/src/components/footer.js
@@ -10,21 +10,23 @@ import {BsArrowRight} from 'react-icons/bs'
 const Footer = () => {
     return ( 
         <div className='col-start-1 col-end-7'>
-            <div className='flex flex-col items-center bg-black'>
-                <img className='pt-14 w-44 mb-8' src={Logo} alt='logo'/>
-                <div className='flex gap-3 justify-items-center pb-12'>
+            <div className='grid grid-cols-6 auto-rows-min bg-black'>
+                <img className='pt-14 mb-8 col-start-3 col-end-5' src={Logo} alt='logo'/>
+                <div className='col-start-2 col-end-6 flex gap-3 justify-center pb-12'>
                     <img className='w-5 h-5' src={Facebook} alt='icon'/>
                     <img className='w-5 h-5' src={Youtube} alt='icon'/>
                     <img className='w-5 h-5' src={Twitter} alt='icon'/>
                     <img className='w-5 h-5' src={Pinterest} alt='icon'/>
                     <img className='w-5 h-5' src={Instagram} alt='icon'/>
                 </div>
-                <h2 className="text-xs text-white font-bold">HOME</h2>
-                <h2 className="text-xs text-white font-bold pt-5">FEATURES</h2>
-                <h2 className="text-xs text-white font-bold pt-5">STORIES</h2>
-                <h2 className="text-xs text-white font-bold pt-5">PRICING</h2>
-                <button className='text-white text-xs font-bold flex items-center pt-28 pb-8'>GET AN INVITE<span className='ml-4 text-2xl'><BsArrowRight/></span></button>
-                <p className='text-base text-opacity-50 font-normal text-white pb-14'>Copyright 2019.All Rights Reserved</p>
+                <div className='flex flex-col justify-center col-start-3 col-end-4 pl-10'>
+                    <h2 className="text-xs text-white font-bold">HOME</h2>
+                    <h2 className="text-xs text-white font-bold pt-5">FEATURES</h2>
+                    <h2 className="text-xs text-white font-bold pt-5">STORIES</h2>
+                    <h2 className="text-xs text-white font-bold pt-5">PRICING</h2>
+                </div>
+                <button className='col-start-2 col-end-6 text-white text-xs font-bold flex items-center justify-center pt-28 pb-8'>GET AN INVITE<span className='ml-4 text-2xl'><BsArrowRight/></span></button>
+                <p className='col-start-1 col-end-7 flex justify-center text-base text-opacity-50 font-normal text-white pb-14'>Copyright 2019.All Rights Reserved</p>
             </div>
         </div>
     );
