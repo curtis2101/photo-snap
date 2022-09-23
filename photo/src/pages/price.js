a17dd2a9e53f0a5f88df6755dd30c5a187ca8931
Curtis Sanderson <cw1sander@gmail.com>
2022-09-23 11:32:29 +0200

    added more padding for toggle switch for large screens

--- photo/src/pages/price.js
+++ photo/src/pages/price.js
@@ -21,16 +21,16 @@ const Price = () => {
                 <img src={Woman2} alt='woman with camera' className='hidden lg:hidden md:block col-start-6 col-end-9 w-full h-full'/>
                 <img src={Woman3} alt='woman with camera' className='hidden lg:block col-start-5 col-end-9 w-full h-full'/>
                 <div className='col-start-1 col-end-9 md:row-start-1 lg:col-end-5 md:col-end-6 pl-7 pr-7 md:pl-14 md:pr-14 md:pt-6 lg:pl-28 lg:pr-20 bg-black text-white '>
-                    <h1 className='text-3xl md:text-4xl font-bold pt-16 md:mt-44 md:pt-0 lg:pt-0'>PRICING</h1>
-                    <p className='font-normal text-base md:text-lg opacity-60 pt-4 md:pt-5 lg:pt-7 lg:w-7/12 pb-16 md:pb-0'>Create a your stories, Photosnap is a platform for photographers and visual storytellers. It’s the simple way to create and share your photos.</p>
+                    <h1 className='text-3xl md:text-4xl font-bold pt-16 md:mt-44 md:pt-0 lg:pt-0 '>PRICING</h1>
+                    <p className='font-normal text-base md:text-lg opacity-60 pt-4 md:pt-5 lg:pt-7 lg:w-10/12 pb-16 md:pb-0 lg:mb-44'>Create a your stories, Photosnap is a platform for photographers and visual storytellers. It’s the simple way to create and share your photos.</p>
                 </div>
                 <div className='col-start-1 col-end-9 bg-white'>
-                {toggle? <section className='flex items-center gap-8 pt-16 pb-10 justify-center'>
+                {toggle? <section className='flex items-center gap-8 pt-16 pb-10 lg:pt-40 justify-center'>
                         <p className='text-lg font-bold opacity-50'>Monthly</p>
                         <Switch defaultChecked onClick={()=>setToggle(!toggle)} ripple={false} color="brown" />
                         <p className='text-lg font-bold'>Yearly</p>
                     </section>  : 
-                    <section className='flex items-center gap-8 pt-16 pb-10 justify-center'>
+                    <section className='flex items-center gap-8 pt-16 lg:pt-40 pb-10 justify-center'>
                         <p className='text-lg font-bold'>Monthly</p>
                         <Switch onClick={()=>setToggle(!toggle)} ripple={false} color="brown" />
                         <p className='text-lg font-bold opacity-50'>Yearly</p>
